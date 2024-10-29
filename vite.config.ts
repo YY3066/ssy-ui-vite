30870eec00226227481c4a807ae9e3cf3dc2a0b2
YY <1421390881@qq.com>
2024-10-29 21:22:37 +0800

    feat(custom): add homepage

--- vite.config.ts
+++ vite.config.ts
@@ -1,66 +1,46 @@
+/// <reference types="vitest/config" />
+import type { BuildOptions } from 'vite'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-/// <reference types="vitest/config" />
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
-// import { presetUno, presetAttributify, presetIcons } from "unocss";
-import UnoCSS from 'unocss/vite'
-
-// const rollupOptions = {
-//   external: ['vue', 'vue-router'],
-//   output: {
-//     globals: {
-//       vue: 'Vue',
-//     },
-//   },
-// }
-
+const rollupOptions: BuildOptions['rollupOptions'] = {
+  external: ['vue'], // 将这些模块保留在 bundle 之外
+  output: {
+    globals: {
+      vue: 'Vue',
+    },
+    exports: 'named',
+  },
+}
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue(), vueJsx(), UnoCSS()],
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler',
     },
   },
-  plugins: [
-    vue(),
-    vueJsx(),
-    UnoCSS(),
-  ],
-  // build: {
-  //   rollupOptions,
-  //   minify: false,
-  //   cssCodeSplit: true,
-  //   // 添加库模式配置
-  //   lib: {
-  //     entry: './src/entry.ts',
-  //     name: 'SSYUI',
-  //     fileName: 'ssy-ui',
-  //     // 导出模块格式
-  //     formats: ['es', 'umd', 'iife'],
-  //   },
-  // },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+
   build: {
-    rollupOptions: {
-      output: {
-        exports: 'named',
-      },
-    },
+    rollupOptions,
     minify: 'terser', // boolean | 'terser' | 'esbuild'
-    sourcemap: true, // 输出单独 source文件
+    sourcemap: false, // 输出单独 source文件
     reportCompressedSize: true, // 生成压缩大小报告
     cssCodeSplit: true,
+    // 添加库模式配置
     lib: {
-      entry: './src/entry.ts',
+      entry: resolve(__dirname, 'src/entry.ts'),
       name: 'SSYUI',
       fileName: 'ssy-ui',
-      formats: ['es', 'umd', 'iife'], // 导出模块类型
+      // 导出模块格式
+      formats: ['es', 'umd'],
     },
-  },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
   },
 })
